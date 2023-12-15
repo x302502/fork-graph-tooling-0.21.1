d9befeac2f234470d307f46e4de1b78852a1eb9d
Thanh Luan <thanhluan.bkhn@gmail.com>
2023-12-15 15:42:48 +0700

    update

--- src/command-helpers/compiler.js
+++ src/command-helpers/compiler.js
@@ -1,5 +1,5 @@
 const URL = require('url').URL
-const ipfsHttpClient = require('ipfs-http-client')
+const { create: ipfsHttpClient } = require('ipfs-http-client')
 const toolbox = require('gluegun/toolbox')
 const Compiler = require('../compiler')
 
@@ -14,15 +14,14 @@ const createCompiler = (manifest, { ipfs, outputDir, outputFormat, skipMigration
 The IPFS URL must be of the following format: http(s)://host[:port]/[path]`)
     return null
   }
-
   // Connect to the IPFS node (if a node address was provided)
   ipfs = ipfs
     ? ipfsHttpClient({
-        protocol: url.protocol.replace(/[:]+$/, ''),
-        host: url.hostname,
-        port: url.port,
-        'api-path': url.pathname.replace(/\/$/, '') + '/api/v0/',
-      })
+      protocol: url.protocol.replace(/[:]+$/, ''),
+      host: url.hostname,
+      port: url.port,
+      'api-path': url.pathname.replace(/\/$/, '') + '/api/v0/',
+    })
     : undefined
 
   return new Compiler({
